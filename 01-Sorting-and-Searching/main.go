ee6b9b7348a4de3ae9e3ac6ea1cfd6d433a23058
alema-r <alessandromarcolini99@gmail.com>
2023-06-15 19:46:10 +0200

    Modified main.go to include the `Quicksort` function.
    Now the user has to choose which sorting algorithm wants to use.

--- 01-Sorting-and-Searching/main.go
+++ 01-Sorting-and-Searching/main.go
@@ -6,26 +6,42 @@ import (
 	"time"
 
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/bubblesort"
+	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/quicksort"
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/utils"
+)
+
+const (
+	bubble = iota
+	quick
 )
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	// Get the number of items and maximum item value.
-	var numItems, max int
+	var numItems, max, alg int
 	fmt.Printf("# Items: ")
 	fmt.Scanln(&numItems)
 	fmt.Printf("Max: ")
 	fmt.Scanln(&max)
+	fmt.Printf("Algorithm (0: bubblesort, 1:quicksort): ")
+	fmt.Scanln(&alg)
 
 	// Make and display the unsorted array.
 	arr := utils.MakeRandomArray(numItems, max)
 	utils.PrintArray(arr, numItems)
 	fmt.Println()
 
-	// Sort and display the result.
-	bubblesort.BubbleSort(arr)
+	// Sort with the algorithm chosen, do nothing if number is not 0 or 1
+	switch alg {
+	case bubble:
+		bubblesort.BubbleSort(arr)
+		fmt.Println("Using bubblesort.")
+	case quick:
+		quicksort.Quicksort(arr)
+		fmt.Println("Using quicksort.")
+	}
+	// Display the result
 	utils.PrintArray(arr, numItems)
 
 	// Verify that it's sorted.
