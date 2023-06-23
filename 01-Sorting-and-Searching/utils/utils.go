7f3fa1c8e1a391b7ac068fa8b784f79a87e1d802
alema-r <alessandromarcolini99@gmail.com>
2023-06-23 16:57:04 +0200

    Modified utils to add functions to handle Customer data.

--- 01-Sorting-and-Searching/utils/utils.go
+++ 01-Sorting-and-Searching/utils/utils.go
@@ -3,6 +3,12 @@ package utils
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
+)
+
+const (
+	sorted_str     = "The array is sorted."
+	not_sorted_str = "The array is not sorted."
 )
 
 // MakeRandomArray creates an array of random numbers with numItems elements
@@ -27,9 +33,9 @@ func PrintArray(arr []int, numItems int) {
 // CheckSorted prints to stdout whether the array is sorted or not.
 func CheckSorted(arr []int) {
 	if isSorted(arr) {
-		fmt.Println("The array is sorted.")
+		fmt.Println(sorted_str)
 	} else {
-		fmt.Println("The array is not sorted.")
+		fmt.Println(not_sorted_str)
 	}
 }
 
@@ -37,6 +43,52 @@ func CheckSorted(arr []int) {
 func isSorted(arr []int) bool {
 	for i := 0; i < len(arr)-1; i++ {
 		if arr[i] > arr[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+type Customer struct {
+	Id           string
+	NumPurchases int
+}
+
+// MakeRandomArrayCustomers creates an array of random customers. It uses
+// MakeRandomArray internally
+func MakeRandomArrayCustomers(numItems, max int) []Customer {
+	customerArray := make([]Customer, numItems)
+	arr := MakeRandomArray(numItems, max)
+	for i, v := range arr {
+		customerArray[i] = Customer{Id: "C" + strconv.Itoa(i), NumPurchases: v}
+	}
+	fmt.Println(customerArray)
+	return customerArray
+}
+
+// PrintArrayCustomers prints to stdout an array of Customer.
+func PrintArrayCustomers(arr []Customer, numItems int) {
+	if numItems > len(arr) {
+		fmt.Println(arr)
+	} else {
+		fmt.Println(arr[:numItems])
+	}
+}
+
+// CheckSortedCustomer prints to stdout whether an array of Customer is
+// sorted or not.
+func CheckSortedCustomer(arr []Customer) {
+	if isSortedCustomer(arr) {
+		fmt.Println(sorted_str)
+	} else {
+		fmt.Println(not_sorted_str)
+	}
+}
+
+// isSortedCustomer checks if an array of Customer is sorted or not.
+func isSortedCustomer(arr []Customer) bool {
+	for i := 0; i < len(arr)-1; i++ {
+		if arr[i].NumPurchases > arr[i+1].NumPurchases {
 			return false
 		}
 	}
