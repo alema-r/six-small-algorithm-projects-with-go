84ad934c248bae478d1d6e8a57e7f5b29063fe88
alema-r <alessandromarcolini99@gmail.com>
2023-06-24 15:08:56 +0200

    Modified main.go to handle both sorting and searching (Hopefully for the last time :))

--- 01-Sorting-and-Searching/main.go
+++ 01-Sorting-and-Searching/main.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/bubblesort"
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/countingsort"
+	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/linearsearch"
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/quicksort"
 	"github.com/alema-r/six-small-algorithm-projects-with-go/01-Sorting-and-Searching/utils"
 )
@@ -22,39 +23,61 @@ func main() {
 
 	// Get the number of items and maximum item value.
 	var numItems, max, alg int
+	var sorting bool
 	fmt.Printf("# Items: ")
 	fmt.Scanln(&numItems)
 	fmt.Printf("Max: ")
 	fmt.Scanln(&max)
-	fmt.Printf("Algorithm (0: bubblesort, 1:quicksort, 2:countingsort): ")
-	fmt.Scanln(&alg)
+	fmt.Printf("Do you want to search or sort? (search: 0, sort:1): ")
+	fmt.Scanln(&sorting)
 
-	// Sort with the algorithm chosen, do nothing if number is not 0 or 1
-	switch alg {
+	if sorting {
 
-	case bubble:
-		fmt.Println("Using bubblesort.")
+		fmt.Printf("Algorithm (0: bubblesort, 1: quicksort, 2: countingsort): ")
+		fmt.Scanln(&alg)
+
+		// Sort with the algorithm chosen
+		switch alg {
+
+		case bubble:
+			fmt.Println("Using bubblesort.")
+			arr := utils.MakeRandomArray(numItems, max)
+			utils.PrintArray(arr, numItems)
+			bubblesort.BubbleSort(arr)
+			utils.PrintArray(arr, numItems)
+			utils.CheckSorted(arr)
+
+		case quick:
+			fmt.Println("Using quicksort.")
+			arr := utils.MakeRandomArray(numItems, max)
+			utils.PrintArray(arr, numItems)
+			quicksort.Quicksort(arr)
+			utils.PrintArray(arr, numItems)
+			utils.CheckSorted(arr)
+
+		case counting:
+			fmt.Println("Using countingsort.")
+			arr := utils.MakeRandomArrayCustomers(numItems, max)
+			utils.PrintArrayCustomers(arr, numItems)
+			arr = countingsort.CountingSortCustomer(arr, max)
+			utils.PrintArrayCustomers(arr, numItems)
+			utils.CheckSortedCustomer(arr)
+		}
+
+	} else {
+		var target int
 		arr := utils.MakeRandomArray(numItems, max)
 		utils.PrintArray(arr, numItems)
-		bubblesort.BubbleSort(arr)
-		utils.PrintArray(arr, numItems)
-		utils.CheckSorted(arr)
+		for {
+			fmt.Printf("Target: ")
+			fmt.Scanln(&target)
+			index, numTests := linearsearch.LinearSearch(arr, target)
+			if index != -1 {
+				fmt.Printf("array[%d] = %d, %d tests\n", index, target, numTests)
+			} else {
+				fmt.Printf("Target %d not found, %d tests\n", target, numTests)
+			}
+		}
 
-	case quick:
-		fmt.Println("Using quicksort.")
-		arr := utils.MakeRandomArray(numItems, max)
-		utils.PrintArray(arr, numItems)
-		quicksort.Quicksort(arr)
-		utils.PrintArray(arr, numItems)
-		utils.CheckSorted(arr)
-
-	case counting:
-		fmt.Println("Using countingsort.")
-		arr := utils.MakeRandomArrayCustomers(numItems, max)
-		utils.PrintArrayCustomers(arr, numItems)
-		arr = countingsort.CountingSortCustomer(arr, max)
-		utils.PrintArrayCustomers(arr, numItems)
-		utils.CheckSortedCustomer(arr)
 	}
-
 }
